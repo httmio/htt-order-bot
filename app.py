46b267cf6e30b7de3cc00f4ae8f7fadda5a90fbd
HttMio <paul7322000@gmail.com>
2018-04-21 02:51:57 +0800

    test

--- app.py
+++ app.py
@@ -68,8 +68,8 @@ def handle_message(event):
         event.reply_token,
         TextSendMessage(text='雷姆是一位有著水藍色頭髮、水藍色瞳孔的少女，有著與雙胞胎姊姊拉姆相似的外型，右眼以瀏海掩蓋，只露出左眼，與姐姐拉姆相反；胸部則比拉姆大一點'))
     elif event.message.text.find('開團') != -1:
+        shop = event.message.text.split()
         if shop[0] == '開團' :
-            shop = event.message.text.split()
             img = shopMeum(shop[1])
             isCreateOrder = True
             line_bot_api.reply_message(
