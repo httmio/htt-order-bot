6d6f5c87719fd197e2589cf8f18b0e40b1b6f389
HttMio <paul7322000@gmail.com>
2018-04-21 00:59:36 +0800

    bot verify

--- app.py
+++ app.py
@@ -42,10 +42,7 @@ def callback():
     app.logger.info("Request body: " + body)
     print("-----------------------------\n"+body)
     # handle webhook body
-    try:
-        handler.handle(body, signature)
-    except InvalidSignatureError:
-        abort(400)
+  
 
     return 'OK'
 
