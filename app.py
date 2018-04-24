d98175344070b9207fc6011d8e0d6c23be9a9bb7
HttMio <paul7322000@gmail.com>
2018-04-25 02:43:49 +0800

    重新上傳

--- app.py
+++ app.py
@@ -35,7 +35,18 @@ def index():
     return "<p>Hello World!</p>"
 @app.route("/callback", methods=['POST'])
 def callback():
- 
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
+
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+    print("-----------------------------\n"+body)
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
 
     return 'OK'
 
